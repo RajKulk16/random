3795040440bdcddd81b8c81be3a4268799378c19
RajKulk16 <rsk132302@gmail.com>
2023-04-05 00:00:32 +0530

    code to exeption,logger

---
+++ src/exception.py
@@ -0,0 +1,23 @@
+import sys # Any exception that is being controlled, sys will have that information.
+import logging
+import logger
+
+logger.logging.basicConfig()
+
+def error_message_detail(error,error_detail:sys): # Here, error_detail is present in sys module.
+    _,_,exc_tb=error_detail.exc_info()
+    # First 2 return values are not required, so we are using _.
+    # 3rd return value tells information like on which file the exception occurred, on which line, etc.
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error in file {} occurred on line {} with error {}".format(file_name,exc_tb.tb_lineno,str(error))
+    return error_message
+    
+# Whenever error occurs, we will call this function.
+
+class CustomException(Exception):
+    def __init__(self, error_message,error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+        
+    def __str__(self):
+        return self.error_message
---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+# logging.basicConfig is used to configure the logging module or used to create the logging statements or logging 
+# file the way we want it be seen.
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
